119fc22ff063f1ea4d3e5b95e1ba4c0df33973ca
Xavier Kuehn <xkuehn@scu.edu>
2020-10-12 00:21:31 -0700

    Made first practice file

---
+++ practice/store-elements-ex/store-elements.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    int arr[10];
+    int input = 1;
+    
+	for(int i = 0; i < sizeof(arr)/sizeof(input); i++){
+		arr[i] = input;
+		printf("%d ", arr[i]);
+		input++;
+	}
+}
